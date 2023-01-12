528daa15889a5c567c0ab65461c69a7c4c4ecab2
James Humphries <jamesh@spacelift.io>
2023-01-12 13:57:49 +0000

    added output

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+output "password" {
+  sensitive = true
+  value     = random_password.password.result
+}
